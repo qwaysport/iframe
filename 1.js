13eb5301b378dbd79b9827c4c62c20e53b907fec
qwaysport <31787928+qwaysport@users.noreply.github.com>
2022-12-07 21:52:51 +0700

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-(function(){
-                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv2/manifest.mpd";
-                var player = dashjs.MediaPlayer().create();
-                player.initialize(document.querySelector("#videoPlayer"), url, true);
-            })();
