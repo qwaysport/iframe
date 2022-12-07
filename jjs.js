0bc6f742eb1a69ceaf8fb8925745da72043f1411
qwaysport <31787928+qwaysport@users.noreply.github.com>
2022-12-07 21:26:35 +0700

    Create jjs.js

---
+++ jjs.js
@@ -0,0 +1,33 @@
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv1/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayer1"), url, true);
+            })();
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv2/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayer2"), url, true);
+            })();
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:tv6/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayer6"), url, true);
+            })();
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:okey/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayero"), url, true);
+            })();
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:berita/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayerb"), url, true);
+            })();
+ (function(){
+                var url = "https://d25tgymtnqzu8s.cloudfront.net/smil:sukan/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayers"), url, true);
+            })();
+
+
+
